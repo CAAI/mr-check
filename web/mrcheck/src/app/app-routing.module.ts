8ed5cd5aaa143a0dfee464cc828d77e226a3e4a5
Peter McDaniel <kg.mcd@hotmail.com>
2020-12-08 17:16:08 +0100

    update

--- web/mrcheck/src/app/app-routing.module.ts
+++ web/mrcheck/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
 
-const routes: Routes = [
-  {path: "/chart", component: ChartComponent}
-];
+const routes: Routes = [{ path: 'chart', component: ChartComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- web/mrcheck/src/app/chart/chart.component.ts
+++ web/mrcheck/src/app/chart/chart.component.ts
@@ -1,56 +1,88 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import {Chart} from "chart.js";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.sass']
+  styleUrls: ['./chart.component.sass'],
 })
 export class ChartComponent implements OnInit, AfterViewInit {
-  @ViewChild("chartCanvas") barCanvas: ElementRef;
-  barChart: any;
+  @ViewChild('chartCanvas') barCanvas: ElementRef<HTMLCanvasElement>;
+  barChart: Chart;
+  labels: Array<string>;
+  data: number[];
+  bgColors: String[];
+  borderColors: String[];
+  finalData: Object[];
 
-  constructor() { }
-
-  ngOnInit(): void {
-    
+  constructor() {
+    this.labels = ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'];
+    this.data = [200, 50, 30, 15, 20, 34];
+    this.bgColors = [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+    ];
+    this.borderColors = [
+      'rgba(255,99,132,1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)',
+      'rgba(153, 102, 255, 1)',
+      'rgba(255, 159, 64, 1)',
+    ];
+    this.finalData = [];
+    for (var i = 0; i < this.data.length; i++) {
+      this.finalData.push({
+        
+        y: this.data[i],
+        x: this.labels[i],
+        backgroundColor: this.bgColors[i],
+      });
+    }
+    console.log(this.finalData);
   }
-  ngAfterViewInit() : void {
-    this.barChart = new Chart(this.barCanvas.nativeElement., {
+
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    console.log('charting');
+    this.barChart = new Chart(this.barCanvas.nativeElement.id, {
       type: 'bar',
+      
       data: {
-        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
-        datasets: [{
-          label: '# of Votes',
-          data: [200, 50, 30, 15, 20, 34],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
+        labels: this.labels,
+        datasets: [
+          {
+            label: '# of Votes',
+            data: this.finalData,
+            borderWidth: 1,
+            
+          },
+        ],
       },
       options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    })
-  }
 
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+        onClick: function (c, i) {
+          console.log(c, i);
+        },
+      },
+    });
+  }
 }
